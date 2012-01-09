6c4c42ceb205be4a63e249754f59ef6b0e75a39d
polinarkos <johngoodman@telenet.be>
2012-01-09 23:23:29 +0100

    Added some logic to show custom page header on all pages except home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,21 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @tagline = ""
   end
 
   def contact
     @title = "Contact"
+    @tagline = "Where to find us" 
   end
 
   def about
     @title = "About"
+    @tagline = "Get to know us"
   end
   
   def help
     @title = "Help"
+    @tagline = "Let us help you"
   end
 end
