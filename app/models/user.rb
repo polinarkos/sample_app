7e2a4f8ee7d071cfac661ca19ce446cd286acadc
polinarkos <johngoodman@telenet.be>
2012-01-10 07:38:58 +0100

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+
+  email_regex = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/i
+
+  validates :name,  :presence     => true,
+                    :length       => { :maximum => 50 }
+  validates :email, :presence     => true,
+                    :format       => { :with => email_regex },
+                    :uniqueness   => { :case_sensitive => false }
+                
+
+  
 end
 
 # == Schema Information
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,62 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do 
+    @attr = {:name => "Testyser", :email => "user@example.com"}
+  end
+
+  it "should create a new instance given a valid attribute" do
+    User.create!(@attr)
+  end
+
+  it "shoudl require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+
+  it "shoudl reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[foo@bar.org bar@lst.la.com baz.list@kak.jp]
+    addresses.each do |a|
+      valid_email_user = User.new(@attr.merge(:email => a))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[foo@bar,org bar_at_lst.la.com baz.list@kak.]
+    addresses.each do |a|
+      invalid_email_user = User.new(@attr.merge(:email => a))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_dup_email = User.new(@attr)
+    user_with_dup_email.should_not be_valid
+  end
+
+  it "shoudl reject email addresses identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_dup_email = User.new(@attr)
+    user_with_dup_email.should_not be_valid
+  end
+  
+
 end
 
 # == Schema Information
