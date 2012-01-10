2778262f2de657e7e99e7cc04a2b103f27229f3d
polinarkos <johngoodman@telenet.be>
2012-01-10 12:47:37 +0100

    added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,13 @@ class UsersController < ApplicationController
 
 
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
-
+  def create
+    @user = User.new
+    render 'new'
+  end
 
 end
