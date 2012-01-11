ef20c738985e2147d26bfa31bb57cf314d2c55e9
polinarkos <johngoodman@telenet.be>
2012-01-11 12:37:22 +0100

    handling invalid signing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    loggedin = User.authenticate(params[:session][:email],
+                                 params[:session][:password])
+    if loggedin
+      redirect_to user_path(loggedin)
+    else
+      flash.now[:error] = "Invalid email/password combination"
+      @title = "Sign in"
+      render 'new'
+    end    
   end
 
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,6 +13,22 @@ describe SessionsController do
       get :new
       response.should have_selector("title", :content => "Sign in")
     end
-  end
 
+    describe "POST create" do
+      describe "failure" do 
+        before(:each) do
+          @attr = { :email => "", :password => "" }
+        end
+        it "should rerender the new page" do
+          post :create, :session => @attr
+          response.should have_selector("title", :content => "Sign in")
+          response.should render_template('new')
+        end
+        it "should have an error message" do 
+          post :create, :session => @attr
+          flash.now[:error].should =~ /invalid/i
+        end
+      end
+    end
+  end
 end
